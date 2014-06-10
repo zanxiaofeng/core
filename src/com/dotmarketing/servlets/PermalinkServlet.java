adc9c46ac93fed590d769861a149542bccef6780
Will Ezell <will@dotcms.com>
2014-06-10 13:26:23 -0400

    #5759 fixed the permalink servlet
    Former-commit-id: 4df68e1401cab708607d502d84e421273e8d5e3d

--- src/com/dotmarketing/servlets/PermalinkServlet.java
+++ src/com/dotmarketing/servlets/PermalinkServlet.java
@@ -14,15 +14,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.dotcms.util.SecurityUtils;
+import com.dotmarketing.beans.Host;
 import com.dotmarketing.beans.Identifier;
 import com.dotmarketing.beans.Trackback;
 import com.dotmarketing.business.APILocator;
 import com.dotmarketing.business.IdentifierCache;
+import com.dotmarketing.business.web.WebAPILocator;
 import com.dotmarketing.factories.InodeFactory;
 import com.dotmarketing.factories.TrackbackFactory;
 import com.dotmarketing.portlets.contentlet.business.ContentletAPI;
 import com.dotmarketing.portlets.contentlet.model.Contentlet;
 import com.dotmarketing.portlets.htmlpages.model.HTMLPage;
+import com.dotmarketing.portlets.languagesmanager.model.Language;
 import com.dotmarketing.util.Config;
 import com.dotmarketing.util.InodeUtils;
 import com.dotmarketing.util.Logger;
@@ -30,15 +33,19 @@ import com.dotmarketing.util.UtilMethods;
 import com.liferay.portal.model.User;
 
 /**
- * Save the trackback pin request or Redirect the permanentlink href macros.
- * If the passed identifier belongs to an html page the servlet just have to forward the page url. E.G.
- * http://dotcms.org/permalink/123 -> 123 = /home/products.dot -> forward to /home/products.dot
- * If the passed identifier belongs to a file the servlet just have to forward to the speedy assets servlet (/dotAsset/{identifier}.{ext})
- * http://dotcms.org/permalink/123 -> 123 = /home/myfile.pdf -> forward to /dotAsset/123.pdf
- * If the passed inode is a contentlet then servlet should use the second parameter, the detail page identifier
- * to the determine where to forward and pass the given identifier that page. E.G.
- * http://dotcms.org/permalink/123/567 -> 123 = My News Release Content, 567 = /home/news_detail_page.dot
- * -> forward to /home/news_detail_page.dot?id=123 
+ * Save the trackback pin request or Redirect the permanentlink href macros. If
+ * the passed identifier belongs to an html page the servlet just have to
+ * forward the page url. E.G. http://dotcms.org/permalink/123 -> 123 =
+ * /home/products.dot -> forward to /home/products.dot If the passed identifier
+ * belongs to a file the servlet just have to forward to the speedy assets
+ * servlet (/dotAsset/{identifier}.{ext}) http://dotcms.org/permalink/123 -> 123
+ * = /home/myfile.pdf -> forward to /dotAsset/123.pdf If the passed inode is a
+ * contentlet then servlet should use the second parameter, the detail page
+ * identifier to the determine where to forward and pass the given identifier
+ * that page. E.G. http://dotcms.org/permalink/123/567 -> 123 = My News Release
+ * Content, 567 = /home/news_detail_page.dot -> forward to
+ * /home/news_detail_page.dot?id=123
+ * 
  * @author Oswaldo Gallango
  * @version 1.0
  * @since 1.5
@@ -51,9 +58,7 @@ public class PermalinkServlet extends HttpServlet {
 
 	}
 
-
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-	throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		if (Config.CONTEXT == null) {
 			Logger.debug(this, "Link not Found");
@@ -61,106 +66,91 @@ public class PermalinkServlet extends HttpServlet {
 			return;
 		}
 
-		String serverName = request.getServerName();
-		int port = request.getServerPort();
-		String redirect ="http://" + serverName;
-		if(port != 80)
-		{
-			redirect += ":" + port;
+		// if this is a blog trackback
+		if (UtilMethods.isSet(request.getParameter("blog_name")) || UtilMethods.isSet(request.getParameter("excerpt"))
+				|| UtilMethods.isSet(request.getParameter("title")) || UtilMethods.isSet(request.getParameter("url"))) {
+
+			processTrackBackPing(request, response);
+			return;
 		}
 
-		StringTokenizer urlString = new StringTokenizer(request.getRequestURI(),"/");
+		String serverName = request.getServerName();
+
+		StringTokenizer urlString = new StringTokenizer(request.getRequestURI(), "/");
 		urlString.nextToken();
 
-		String _inode = null;
-		String _page = null;
+		String id = urlString.nextToken();
 
-		_inode = urlString.nextToken();
-		if(urlString.hasMoreTokens()){
-			_page = urlString.nextToken();
-		}
-
-		Identifier iden = null;
 		try {
-			iden = APILocator.getIdentifierAPI().find(_inode);
-		} catch (Exception e) {
-			Logger.info(PermalinkServlet.class, e.getMessage());
-		}
-
-		/**
-		 * Check if is a trackback ping calling from the permalink url
-		 * The blog_name, title, excerpt, url params could be only use for
-		 * call to trackback process
-		 */
-		if(UtilMethods.isSet(request.getParameter("blog_name")) || UtilMethods.isSet(request.getParameter("excerpt")) ||
-				UtilMethods.isSet(request.getParameter("title")) ||	UtilMethods.isSet(request.getParameter("url"))){
-				
-				processTrackBackPing(request,response);
-				return;
-		} else{
-			/**
-			 * Call to the usual permalink functionality redirecting to the page or file location
-			 */
-			/*Check if is a contentlet and redirect to the specified page*/
-			ContentletAPI contentletApi = APILocator.getContentletAPI();
-			String luceneQuery = "+type:content +live:true +deleted:false +identifier:" + iden.getInode();
-			
-			List<Contentlet> contentlets = new ArrayList<Contentlet>();
-			try
-			{
-				User user = APILocator.getUserAPI().getSystemUser();
-				contentlets = contentletApi.search(luceneQuery,0,0,"",user,false);
+			User user = WebAPILocator.getUserWebAPI().getLoggedInFrontendUser(request);
+			Host host = WebAPILocator.getHostWebAPI().resolveHostName(serverName, user, true);
+			Identifier identifier = null;
+			try {
+				identifier = APILocator.getIdentifierAPI().find(id);
+			} catch (Exception e) {
+				throw new Exception("No valid Identifier found");
 			}
-			catch(Exception ex)
-			{
-				Logger.info(PermalinkServlet.class,ex.getMessage());
+			if (identifier == null || !UtilMethods.isSet(identifier.getId())) {
+				throw new Exception("No valid Identifier found");
 			}
-			
-			if(contentlets.size() > 0)
-			{
-				try {
-					//_page should be an identifier
-					Identifier pageIden = APILocator.getIdentifierAPI().find(_page);
-					//in case it is the inode of the page
-					if(!InodeUtils.isSet(pageIden.getInode())){
-						pageIden = APILocator.getIdentifierAPI().find((HTMLPage)InodeFactory.getInode(_page, HTMLPage.class));
-					}
-					redirect = redirect+UtilMethods.encodeURIComponent(pageIden.getURI())+"?id="+_inode;
 
-				} catch (Exception e) {
-					Logger.info(PermalinkServlet.class, e.getMessage());
+			if ("contentlet".equalsIgnoreCase(identifier.getAssetType())) {
+				ContentletAPI capi = APILocator.getContentletAPI();
+				Language l = WebAPILocator.getLanguageWebAPI().getLanguage(request);
+
+				Contentlet c = (Contentlet) capi.findContentletByIdentifier(identifier.getId(), true, l.getId(), user, true);
+				if (c == null || !UtilMethods.isSet(c.getInode())) {
+					throw new Exception("No valid Contentlet found");
+				}
+				String x = capi.getUrlMapForContentlet(c, user, true);
+				if ("URL_MAP_FOR_CONTENT_404".equals(x)) {
+					throw new Exception("No valid urlmap found");
 				}
 
-			} else if(iden.getURI().endsWith(Config.getStringProperty("VELOCITY_PAGE_EXTENSION")) ||
-					iden.getURI().endsWith(Config.getStringProperty("VELOCITY_HTMLPAGE_EXTENSION"))){
-				/*Check if is a page and redirect to the specified page*/
-				redirect = redirect+UtilMethods.encodeURIComponent(iden.getURI());
-			}else{
-				/*Check if is a contentlet and redirect to the speedy asset servlet */
-				redirect = redirect+"/dotAsset/"+iden.getInode()+iden.getURI().substring(iden.getURI().lastIndexOf("."));
+				// if the content is on the system host (shared) or on the
+				// current host
+				if (c.getHost().equals(APILocator.getHostAPI().findSystemHost().getIdentifier())
+						|| c.getHost().equals(host.getIdentifier())) {
+					response.sendRedirect(x);
+					return;
+				}
+
 			}
 
+			if ("htmlpage".equalsIgnoreCase(identifier.getAssetType())) {
+				if (host.getIdentifier().equals(identifier.getHostId())) {
+					response.sendRedirect(identifier.getURI());
+					return;
+
+				}
+			}
+
+		} catch (Exception ex) {
+
+			Logger.error(this, "PERMALink not Found id:" + id + " : " + ex);
 
 		}
-		response.sendRedirect(SecurityUtils.stripReferer(request, redirect));
+		response.sendError(404, "PermaLink not Found");
 
 	}
 
 	/**
-	 * This method process the trackback ping  saving in db the trackback if the info is correct and return
-	 * a XML message indicating if the ping was successfull or the error
+	 * This method process the trackback ping saving in db the trackback if the
+	 * info is correct and return a XML message indicating if the ping was
+	 * successfull or the error
+	 * 
 	 * @param request
 	 * @param response
 	 * @throws ServletException
 	 * @throws IOException
 	 */
-	public void processTrackBackPing(HttpServletRequest request, HttpServletResponse response) 
-	throws ServletException, IOException {
+	public void processTrackBackPing(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+			IOException {
 
 		StringBuffer message = new StringBuffer();
 		ServletOutputStream out = response.getOutputStream();
 		try {
-			StringTokenizer urlString = new StringTokenizer(request.getRequestURI(),"/");
+			StringTokenizer urlString = new StringTokenizer(request.getRequestURI(), "/");
 			urlString.nextToken();
 
 			String _identifier = null;
@@ -168,18 +158,18 @@ public class PermalinkServlet extends HttpServlet {
 			_identifier = urlString.nextToken();
 			Trackback tb = new Trackback();
 			tb.setAssetIdentifier(_identifier);
-			if(UtilMethods.isSet(request.getParameter("blog_name"))){
+			if (UtilMethods.isSet(request.getParameter("blog_name"))) {
 				tb.setBlogName(request.getParameter("blog_name"));
 			}
-			if(UtilMethods.isSet(request.getParameter("excerpt"))){
+			if (UtilMethods.isSet(request.getParameter("excerpt"))) {
 				tb.setExcerpt(request.getParameter("excerpt"));
 			}
-			if(UtilMethods.isSet(request.getParameter("title"))){
+			if (UtilMethods.isSet(request.getParameter("title"))) {
 				tb.setTitle(request.getParameter("title"));
 			}
-			if(UtilMethods.isSet(request.getParameter("url"))){
+			if (UtilMethods.isSet(request.getParameter("url"))) {
 				tb.setUrl(request.getParameter("url"));
-			} else{
+			} else {
 				message.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
 				message.append("<response><error>1</error><message>");
 				message.append("url parameter is required");
@@ -195,8 +185,7 @@ public class PermalinkServlet extends HttpServlet {
 			message.append("</response>");
 			out.print(message.toString());
 
-
-		}catch (Exception e) {
+		} catch (Exception e) {
 
 			message.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
 			message.append("<response><error>1</error><message>");
@@ -204,10 +193,9 @@ public class PermalinkServlet extends HttpServlet {
 			message.append("</message></response>");
 			out.print(message.toString());
 
-		}finally{
+		} finally {
 			out.close();
 		}
 	}
-
 
 }
