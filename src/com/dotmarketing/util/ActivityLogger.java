df2cc6e7fb3db4b4a91b6c997822ebb2687a8337
Samuel Mortha <sam.mortha@dotcms.com>
2014-10-01 01:03:26 +0530

    #6400

--- src/com/dotmarketing/util/ActivityLogger.java
+++ src/com/dotmarketing/util/ActivityLogger.java
@@ -1,5 +1,7 @@
 package com.dotmarketing.util;
 
+import com.dotmarketing.beans.Host;
+import com.dotmarketing.business.APILocator;
 import com.dotmarketing.logConsole.model.LogMapper;
 
 public class ActivityLogger {
@@ -15,15 +17,31 @@ public class ActivityLogger {
 		if (LogMapper.getInstance().isLogEnabled(filename)) {
 			if (!UtilMethods.isSet(host)) {
 				host = "system";
+			}else{
+				try {
+					Host h = APILocator.getHostAPI().find(host, APILocator.getUserAPI().getSystemUser(), false);
+					if(h != null)
+						host = h.getHostname();
+				} catch (Exception e) {}
 			}
-			Logger.info(ActivityLogger.class, cl.toString() + ": " + host
+				Logger.info(ActivityLogger.class, cl.toString() + ": " + host
 					+ " : " + action + " , " + msg);
-		}
+			}
 	}
 
 	public static void logDebug(Class cl, String action, String msg, String host) {
 
 		if (LogMapper.getInstance().isLogEnabled(filename)) {
+			if (!UtilMethods.isSet(host)) {
+				host = "system";
+			} else {
+				try {
+					Host h = APILocator.getHostAPI().find(host,
+							APILocator.getUserAPI().getSystemUser(), false);
+					if (h != null)
+						host = h.getHostname();
+				} catch (Exception e) {}
+			}
 			Logger.debug(ActivityLogger.class, cl.toString() + ": " + host
 					+ " :" + action + " , " + msg);
 		}
